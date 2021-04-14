ce1f189449295fcc9aaf44764944354e193832d1
GuiGallagher <cobaincorporations@alunos.fho.edu.br>
2021-04-14 20:41:03 -0300

    style js

--- style.js
+++ style.js
@@ -1,7 +1,15 @@
 function salgadinho{
     var selecionar = document.getElementById("opc").value;
     
+    if(selecionar == "coxinha"){
+        var n1 = parseInt(document.getElementById("v1").value);
+        var n2 = parseInt(document.getElementById("v2").value);
+        var res = (n1 + n2);
+        alert(res);
+        alert("a operação foi de soma");
+    }
     
-    aaaa
+    
+
     
 }

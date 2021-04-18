ac718166949cb5284e9b38a0f9d5273fabb2dbf4
lucas41 <32311187+lucas41@users.noreply.github.com>
2021-04-18 02:24:49 -0300

    projeto final

--- js/index.js
+++ js/index.js
@@ -1,36 +1,63 @@
-function produtos() {
+function produtos(el) {
 
    
     var input = document.querySelector("#qtdeBolinha");
     var bolinha = input.value;
-    var bolinha = parseInt(bolinha) * 4;
+    var bolinha = parseFloat(bolinha) * 4.00;
 
     var input = document.querySelector("#qtdcoxinha");
     var coxinha = input.value;
-    var coxinha = parseInt(coxinha) * 5;
+    var coxinha = parseFloat(coxinha) * 5.00;
 
     var input = document.querySelector("#qtdenrolado");
     var enrolado = input.value;
-    var enrolado = parseInt(enrolado) * 3;
+    var enrolado = parseFloat(enrolado) * 3.00;
 
     var input = document.querySelector("#qtdcoca");
     var coca = input.value;
-    var coca = parseFloat(coca) * 4.5;
+    var coca = parseFloat(coca) * 4.50;
 
     var input = document.querySelector("#qtdcafe");
     var cafe = input.value;
-    var cafe = parseInt(cafe) * 3;
+    var cafe = parseFloat(cafe) * 3.00;
 
     var input = document.querySelector("#qtdagua");
     var agua = input.value;
-    var agua = parseInt(agua) * 2;
+    var agua = parseFloat(agua) * 2.00;
 
     
 
-    var soma = parseInt(bolinha) + parseInt(coxinha) + parseInt(enrolado) + parseFloat(coca) + parseInt(cafe) + parseInt(agua);
-    alert(soma);
+    var soma = parseFloat(bolinha) + parseFloat(coxinha) + parseFloat(enrolado) + parseFloat(coca) + parseFloat(cafe) + parseFloat(agua);
+
+    if(document.getElementById("estudante").value == "Sim") {
+        desconto = parseFloat(soma) * 0.05;
+        soma = soma - desconto;
+        console.log(soma);
+    } else {
+        console.log(soma);
+    }
 
 
+
+    if(document.getElementById("viagem").value == "Sim") {
+        soma = parseFloat(soma) + 1.50;
+        console.log(soma);
+    } else {
+        console.log(soma);
+    }
+
+
+
+    document.getElementById("campo1").innerHTML = 'R$: '+soma;
     
+    var display = document.getElementById(el).style.display;
+    if (display == "none")
+      document.getElementById(el).style.display = 'block';
+    else
+      document.getElementById(el).style.display = 'none';
+
 }
+
+
+
 
